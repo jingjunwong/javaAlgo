5051e083656553408ae182a8bc2b3543c5593e3c
Wong Jing Jun <adrianwong35@gmail.com>
2024-01-03 21:58:09 +0800

    added ReverseWords.java

---
+++ app/src/main/java/SortingAlgo/ReverseWords.java
@@ -0,0 +1,51 @@
+package SortingAlgo;
+
+//Reversing words in an array containing characters, eg. "Sky Blue" to "Blue Sky"
+public class ReverseWords {
+    public static void main(String[] args)
+    {
+        char[] words = {'b','l','u','e',' ','s','k','y'};
+
+        reverseWords(words);
+        reverseIndividualWords(words);
+        System.out.println(words);
+    }
+
+    public static void reverseWords(char[] words)
+    {
+        reverseAllCharacters(0, words.length-1, words);
+    }
+
+    public static void reverseAllCharacters(int i, int j, char[] words)
+    {
+        while(i<j)
+        {
+            //swapCharacters(i++,j--,words) 'this code works too'
+            swapCharacters(i, j, words);
+            i++;
+            j--;
+        }
+    }
+
+    public static void swapCharacters(int i, int j, char[] words)
+    {
+        char temp = words[i];
+        words[i] = words[j];
+        words[j] = temp;
+    }
+
+    public static void reverseIndividualWords(char[] words)
+    {
+        int leftPointer = 0;
+        int rightPointer = words.length;
+
+        for(int i = 0; i<words.length; i++)
+        {
+            if(words[i] != ' ' && (i == rightPointer - 1 || words[i+1] == ' '))
+            {
+                reverseAllCharacters(leftPointer, i, words);
+                leftPointer = i + 2;
+            }
+        }
+    }
+}
